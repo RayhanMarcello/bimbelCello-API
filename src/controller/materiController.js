dbc522423277aedd4da73d8315094e52d623956b
Rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2025-12-09 02:20:14 +0800

    membuat controller get all user

--- src/controller/materiController.js
+++ src/controller/materiController.js
@@ -3,7 +3,7 @@ import materiModel from "../models/materiModel.js";
 const uploadMateri = async (req, res) => {
   try {
     const { judul, deskripsi, owner_id } = req.body;
-    await materiModel(judul, deskripsi, owner_id);
+    await materiModel.uploadMateri(judul, deskripsi, owner_id);
     res.json({
       message: "succsess upload materi",
       data: {
@@ -18,4 +18,18 @@ const uploadMateri = async (req, res) => {
   }
 };
 
-export default uploadMateri;
+const getAllMateri = async (req, res) => {
+  try {
+    const [data] = await materiModel.getAllMateri();
+    res.json({
+      message: "sucsess menampilkan semua materi",
+      data: data,
+    });
+  } catch (error) {
+    res.json({
+      message: error.message,
+    });
+  }
+};
+
+export default { uploadMateri, getAllMateri };
