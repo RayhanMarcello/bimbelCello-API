8e1ce1d8f67c38853d680cbfea585390431cc2a8
Rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2025-12-09 01:49:37 +0800

    menambahkan endpoint materi

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import routesLogin from "./src/routes/login.js";
 import routesRegister from "./src/routes/register.js";
 import routesUsers from "./src/routes/users.js";
+import routesMateri from "./src/routes/materi.js";
 
 configDotenv();
 const app = express();
@@ -13,6 +14,7 @@ const port = process.env.PORT;
 app.use("/api/login", routesLogin);
 app.use("/api/register", routesRegister);
 app.use("/api/users", routesUsers);
+app.use("/api/materi", routesMateri);
 
 app.listen(port, () => {
   console.log("server runing at", port);
