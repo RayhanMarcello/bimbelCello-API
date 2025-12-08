48679df94fe9d8da169bc68f4db6343a8522c5be
Rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2025-12-09 02:21:01 +0800

    memanggil sp get all user

--- src/models/materiModel.js
+++ src/models/materiModel.js
@@ -6,4 +6,10 @@ const uploadMateri = async (judul, deskripsi, owner_id) => {
   return result;
 };
 
-export default uploadMateri;
+const getAllMateri = async () => {
+  const sql = "call sp_get_all_materi()";
+  const [result] = await db.execute(sql);
+  return result;
+};
+
+export default { uploadMateri, getAllMateri };
