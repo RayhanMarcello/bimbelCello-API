e1494908a903f1005b64485bb4d0657d13033859
Rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2025-12-09 22:40:07 +0800

    memperbaiki model

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -6,9 +6,9 @@ const register = async (username, password, role) => {
   return result;
 };
 
-const login = async (username, password) => {
-  const sql = "call sp_login_user(?,?)";
-  const [result] = await db.execute(sql, [username, password]);
+const login = async (username, password, role) => {
+  const sql = "call sp_login_user(?,?,?)";
+  const [result] = await db.execute(sql, [username, password, role]);
   return result;
 };
 
