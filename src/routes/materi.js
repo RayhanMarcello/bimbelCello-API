27d5e5e40ef16b2a007ad28006a2208980edeeae
Rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2025-12-09 13:11:42 +0800

    menambahkan methode delete

--- src/routes/materi.js
+++ src/routes/materi.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/", materiController.uploadMateri);
 router.get("/", materiController.getAllMateri);
+router.delete("/:id", materiController.deleteMateri);
 
 export default router;
