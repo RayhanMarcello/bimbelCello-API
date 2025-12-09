238d0dc3b31456105b9bc9d1e2dbcf3714a0eb91
Rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2025-12-10 00:54:28 +0800

    fix detail

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
 import usersController from "../controller/usersController.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/:id", usersController.getUserById);
+router.get("/:id", [auth.isAuthenticated], usersController.getUserById);
 
 export default router;
