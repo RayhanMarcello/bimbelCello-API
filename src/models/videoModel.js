4d6ad24c2be5cf8e7085946309618dec31d1133f
Rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2025-12-10 21:59:04 +0800

    memanggil sp delete video

--- src/models/videoModel.js
+++ src/models/videoModel.js
@@ -10,4 +10,11 @@ const addVideo = async (materi_id, judul, video_path, deskripsi) => {
   ]);
   return result;
 };
-export default { addVideo };
+
+const deleteVideo = async (id) => {
+  const sql = "call sp_delete_video(?)";
+  const [result] = await db.execute(sql, [id]);
+  return result;
+};
+
+export default { addVideo, deleteVideo };
