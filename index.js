0740b4709f80193945a92d5a0ff9a2925f6a89dd
Rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2025-12-10 21:57:34 +0800

    menambahkan rootes video

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import routesLogin from "./src/routes/login.js";
 import routesRegister from "./src/routes/register.js";
 import routesUsers from "./src/routes/users.js";
 import routesMateri from "./src/routes/materi.js";
+import routesVideo from "./src/routes/video.js";
 
 configDotenv();
 const app = express();
@@ -15,6 +16,7 @@ app.use("/api/login", routesLogin);
 app.use("/api/register", routesRegister);
 app.use("/api/users", routesUsers);
 app.use("/api/materi", routesMateri);
+app.use("/api/video", routesVideo);
 
 app.listen(port, () => {
   console.log("server runing at", port);
