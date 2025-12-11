fb02179ef47d29eb3caade35ac47e287ce3f2809
Rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2025-12-12 02:14:22 +0800

    fix soal model

--- src/models/soalModel.js
+++ src/models/soalModel.js
@@ -22,4 +22,22 @@ const addSoal = async (
   return result;
 };
 
-export default { addSoal };
+const deleteSoal = async (idSoal) => {
+  const sql = "call sp_delete_soal(?)";
+  const [result] = await db.execute(sql, [idSoal]);
+  return result;
+};
+
+const getSoalByLatihan = async (idLatihan) => {
+  const sql = "call sp_get_soal_by_latihan(?)";
+  const [result] = await db.execute(sql, [idLatihan]);
+  return result[0];
+};
+
+const getSoalDetail = async (idSoal) => {
+  const sql = "call sp_get_soal_detail(?)";
+  const [result] = await db.execute(sql, [idSoal]);
+  return result[0];
+};
+
+export default { addSoal, deleteSoal, getSoalByLatihan, getSoalDetail };
