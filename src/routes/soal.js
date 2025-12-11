2d4e403015b88b821949beb8f637cedba0923438
Rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2025-12-12 02:07:54 +0800

    menambhakan get soal detail dan get soal by latihan

--- src/routes/soal.js
+++ src/routes/soal.js
@@ -1,9 +1,26 @@
 import express from "express";
 import soalController from "../controller/soalController.js";
-// import auth from "../middleware/auth.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/", soalController.addSoal);
+router.post(
+  "/",
+  [auth.isAuthenticated, auth.isPengajar],
+  soalController.addSoal
+);
+router.delete(
+  "/:idSoal",
+  [auth.isAuthenticated, auth.isPengajar],
+  soalController.deleteSoal
+);
+
+router.get(
+  "/:idLatihan",
+  [auth.isAuthenticated],
+  soalController.getSoalByLatihan
+);
+
+router.get("/detail/", soalController.getSoalDetail);
 
 export default router;
